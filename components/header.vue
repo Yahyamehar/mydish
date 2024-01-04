c149d11d3acece0a0d9273773bfb076616f3a134
Yahyamehar <147187179+Yahyamehar@users.noreply.github.com>
2024-01-04 00:10:19 +0000

    mobile friendly header

--- components/header.vue
+++ components/header.vue
@@ -1,33 +1,53 @@
 <template>
     <header class="header">
-        <div class="header-top">
-            <div class="mobile-header d-flex justify-content-between align-items-center d-xl-none">
+        <!-- Mobile Header -->
+        <div class="mobile-header d-md-none">
+            <div class="logo-container">
                 <NuxtLink to="/" class="logo"><img :src="companyInfo?.main_logo" alt="logo" height="70" width="70"
                         style="border-radius: 34px;"></NuxtLink>
             </div>
-            <div class="d-none d-xl-flex row align-items-center">
-                <div class="col-5 col-md-2">
-                    <NuxtLink to="/" class="logo"><img src="assets/mydish.jpeg" alt="logo" height="70" width="70"
-                            style="border-radius: 34px;"></NuxtLink>
-                </div>
-                <div class="col-2 col-md-1 col-lg-5">
-                    <ul class="site-action d-none d-lg-flex align-items-center justify-content-between ml-auto">
-                        <li class="site-phone" @click="confirmCall">
-                            <a><i class="fas fa-phone"></i> {{ companyInfo?.phone_number }}</a>
-                        </li>
-                        <li class="site-help">
-                            <NuxtLink to="contact"><i class="fas fa-question-circle"></i> Help & More</NuxtLink>
-                        </li>
-                        <li v-if="isLoggedIn">
-                            <a @click="logout"><i class="fas fa-user mr-2"></i>Logout</a>
-                        </li>
-                        <li v-else>
-                            <NuxtLink to="/signin"><i class="fas fa-user mr-2"></i>Sign In</NuxtLink>
-                        </li>
-                    </ul>
-                </div>
+            <div class="menu-container">
+                <ul class="site-action">
+                    <li class="site-phone" @click="confirmCall">
+                        <a><i class="fas fa-phone"></i> {{ companyInfo?.phone_number }}</a>
+                    </li>
+                    <li class="site-help">
+                        <NuxtLink to="contact"><i class="fas fa-question-circle"></i> Help & More</NuxtLink>
+                    </li>
+                    <li class="auth" v-if="isLoggedIn">
+                        <a @click="logout"><i class="fas fa-user mr-2"></i>Logout</a>
+                    </li>
+                    <li class="auth" v-else>
+                        <NuxtLink to="/signin"><i class="fas fa-user mr-2"></i>Sign In</NuxtLink>
+                    </li>
+                </ul>
             </div>
         </div>
+
+        <!-- Desktop Header -->
+        <div class="desktop-header d-none d-md-flex">
+            <div class="logo-container">
+                <NuxtLink to="/" class="logo"><img src="assets/mydish.jpeg" alt="logo" height="70" width="70"
+                        style="border-radius: 34px;"></NuxtLink>
+            </div>
+            <div class="menu-container">
+                <ul class="site-action">
+                    <li class="site-phone" @click="confirmCall">
+                        <a><i class="fas fa-phone"></i> {{ companyInfo?.phone_number }}</a>
+                    </li>
+                    <li class="site-help">
+                        <NuxtLink to="contact"><i class="fas fa-question-circle"></i> Help & More</NuxtLink>
+                    </li>
+                    <li class="auth" v-if="isLoggedIn">
+                        <a @click="logout"><i class="fas fa-user mr-2"></i>Logout</a>
+                    </li>
+                    <li class="auth" v-else>
+                        <NuxtLink to="/signin"><i class="fas fa-user mr-2"></i>Sign In</NuxtLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
         <hr>
 
         <!-- Header Bottom -->
@@ -72,12 +92,12 @@ const companyInfo = ref(null);
 // Function to fetch phone number and logo from the API
 
 onMounted(async () => {
-  try {
-    const response = await axios.get('http://localhost:5000/api/companydetails');
-    companyInfo.value = response.data.companyInfo;
-  } catch (error) {
-    console.error('Error fetching company information:', error.message);
-  }
+    try {
+        const response = await axios.get('http://localhost:5000/api/companydetails');
+        companyInfo.value = response.data.companyInfo;
+    } catch (error) {
+        console.error('Error fetching company information:', error.message);
+    }
 });
 
 const confirmCall = () => {
@@ -128,6 +148,56 @@ onMounted(async () => {
 const showLogout = computed(() => isLoggedIn.value);
 </script>
 
+
+
 <style scoped>
-/* Add any additional styles if needed */
+.header {
+    /* Add any global header styles here */
+}
+
+.mobile-header,
+.desktop-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+    background-color: #ffffff;
+}
+
+.menu-container {
+    flex: 1;
+    display: flex;
+    justify-content: flex-end;
+}
+
+.logo-container,
+.menu-container {
+    flex: 1;
+}
+
+.site-action {
+    list-style: none;
+    display: flex;
+    gap: 10px;
+}
+
+.site-phone a {
+    display: flex;
+    align-items: center;
+}
+
+.site-phone i {
+    margin-right: 5px;
+}
+
+/* Media Query for Mobile */
+@media only screen and (max-width: 767px) {
+    .desktop-header {
+        display: none;
+    }
+
+    .mobile-header {
+        flex-direction: column;
+    }
+}
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,15 +4,15 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
 
   css: [
-    './assets/css/bootstraps.min.css',
-    './assets/css/global.css',
-    './assets/css/all.min.css',
-    './assets/css/animate.css',
-    './assets/css/slick.css',
-    './assets/css/style.css',
-    './assets/css/custom-select.css',
-    './assets/css/slick-theme.css',
-    './assets/css/swiper.min.css',
+    '@/assets/css/bootstraps.min.css',
+    '@/assets/css/global.css',
+    '@/assets/css/all.min.css',
+    '@/assets/css/animate.css',
+    '@/assets/css/slick.css',
+    '@/assets/css/style.css',
+    '@/assets/css/custom-select.css',
+    '@/assets/css/slick-theme.css',
+    '@/assets/css/swiper.min.css',
   ],
 
   build: {
